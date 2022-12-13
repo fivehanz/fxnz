f38bd53f443c3d700671d92b99ef9bb0fd1ea8dd
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-12-13 08:36:32 +0530

    chore: update welcome message for testing

--- routes/routes.go
+++ routes/routes.go
@@ -18,7 +18,7 @@ import (
 func Routes(app *echo.Echo) {
 	// Home Route
 	app.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hey, if you can read this -- ArgoCD is working")
+		return c.String(http.StatusOK, "Hey, if you can read this -- ArgoCD is working -- #2")
 	})
 
 	// Route to redirect the respective URL
