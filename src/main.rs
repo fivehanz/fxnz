79bf7d34c06d158f186234da981444bd9afbfb67
Hanz <22578473+fivehanz@users.noreply.github.com>
2023-01-04 03:28:38 +0530

    chore: test text

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hey, this is written on axum with Rust!!"}));
+    let app = Router::new().route("/", get(|| async { "Hey, if you can see this -- ArgoCD is working #1"}));
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
