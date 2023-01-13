9744da64742824ae6020d4b2ff2b3a94f143e281
Hanz <22578473+fivehanz@users.noreply.github.com>
2023-01-13 15:14:31 +0530

    chore: refactored database.rs & returns DBConn

--- src/database.rs
+++ src/database.rs
@@ -1,14 +1,8 @@
-use sea_orm::{Database, DbErr};
-use migration::{Migrator, MigratorTrait};
+use sea_orm::{Database, DbErr, DatabaseConnection};
 
 
 // connect database 
-pub async fn database_init() -> Result<(), DbErr>{
-    let database_uri = dotenvy::var("DATABASE_URL").unwrap();
-    let db = Database::connect(database_uri).await?;
-
-    Migrator::status(&db).await?;
-
-    Ok(())
+pub async fn database_init() -> Result<DatabaseConnection, DbErr>{
+    Database::connect(dotenvy::var("DATABASE_URL").unwrap()).await
 }
 
