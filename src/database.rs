874d92b8f3978951a9beda7f460235b1104c8d3d
Hanz <22578473+fivehanz@users.noreply.github.com>
2023-01-13 14:08:39 +0530

    chore: migration status in db

--- src/database.rs
+++ src/database.rs
@@ -1,13 +1,13 @@
 use sea_orm::{Database, DbErr};
-// use migration::{Migrator, MigratorTrait};
+use migration::{Migrator, MigratorTrait};
 
 
 // connect database 
 pub async fn database_init() -> Result<(), DbErr>{
     let database_uri = dotenvy::var("DATABASE_URL").unwrap();
-    let _db = Database::connect(database_uri).await?;
+    let db = Database::connect(database_uri).await?;
 
-    // Migrator::up(&db, None).await?;
+    Migrator::status(&db).await?;
 
     Ok(())
 }
